c4d117179ff1b94d74a3b9fd2e921559e8705730
JohnMwaniki-TheJitu <john.mwaniki@griffinglobaltech.com>
2024-09-04 17:04:38 +0300

    feat: create todo ui

--- todo/app/page.tsx
+++ todo/app/page.tsx
@@ -1,11 +1,10 @@
 "use client";
-import Image from "next/image";
 import { Index } from "./pages/Index";
 
 export default function Home() {
   return (
    <>
-   <div className="bg-sky-900">
+   <div>
    <Index/>
    </div>
    </>
--- todo/tailwind.config.ts
+++ todo/tailwind.config.ts
@@ -13,6 +13,10 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        softBlue: "#2B1887",
+        softPurple: '#D5CCFF'
+      }
     },
   },
   plugins: [],
